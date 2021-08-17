168a1b5d9c4612661443ddf6408f4d26c4e7b3a9
Oleksii Malitskyi <oleksii@propertyfinder.ae>
2021-08-17 17:15:27 +0400

    [CX-403] - Added data adapter to social login (#119)

--- src/api/auth/social-login/facebook.fetcher.ts
+++ src/api/auth/social-login/facebook.fetcher.ts
@@ -1,5 +1,6 @@
 import { ApiAuthSocialLoginModelInterface } from 'api/auth/social-login/model.interface';
 import { ApiAuthSocialLoginRequestInterface } from 'api/auth/social-login/request.interface';
+import { ApiAuthSocialLoginResponseInterface } from 'api/auth/social-login/response.interface';
 import { ApiFactory } from 'api/factory';
 import { ApiFetcherResultType } from 'api/fetcher-result-type';
 import { ApiJsonModelInterface } from 'api/json/model.interface';
@@ -7,13 +8,29 @@ import { LocaleService } from 'services/locale/service';
 
 const facebookFetcher = ApiFactory<
   ApiAuthSocialLoginModelInterface,
-  ApiAuthSocialLoginModelInterface,
+  ApiAuthSocialLoginResponseInterface,
   ApiJsonModelInterface<ApiAuthSocialLoginRequestInterface>
 >({
   method: 'POST',
   url: 'user/b2c-login-social/facebook',
   alterHeaders: (headers) => {
     headers['content-type'] = 'application/vnd.api+json';
+  },
+  dataMapper: (data) => {
+    return {
+      user: {
+        email: data.included[0].attributes.email,
+        first_name: data.included[0].attributes.first_name,
+        last_name: data.included[0].attributes.last_name,
+        image: data.included[0].attributes.image,
+        userId: data.data.relationships.user.data.id,
+      },
+      meta: {
+        token: data.data.meta.token,
+        refresh_token: data.data.meta.refresh_token,
+      },
+      email: data.included[0].attributes.email,
+    };
   },
 });
 
