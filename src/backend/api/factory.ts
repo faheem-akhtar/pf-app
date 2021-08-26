649c4f8df7b6369a46cc36eaafa1c852db810568
Oleg Malkov <74129133+pfolegmalkov@users.noreply.github.com>
2021-08-26 17:42:58 +0400

    [CX-206] Clean up search api auth workaround once proper auth flow is in place (#150)
    * [CX-206] Clean up search api auth workaround once proper auth flow is in place
    
    * review changes

--- src/backend/api/factory.ts
+++ src/backend/api/factory.ts
@@ -1,12 +1,10 @@
-import { apiCookieAuthenticatorSignCookie } from 'api/cookie-authenticator/sign-cookie';
 import { backendJsonApiSync } from 'backend/json-api/sync';
 import { configOriginValue } from 'config/origin/value';
 
 import { ApiMakeFactory } from 'api/make-factory';
 import { BackendJsonApiPayloadInterface } from 'backend/json-api/payload.interface';
 
-// TODO-FE[TPNX-3006] delete it once proper auth flow is in place
-const captchaAuthenticatorCookie = '0d5d23b0a8d20d4c43efda4850277f59cc22d534';
+const SECRET_PF_USER_AGENT = 'pf-web-app-74f9c3382b2516b2e826762e13dfa3ea05d84396';
 
 /**
  * This Api factory should be used only to constract the fetchers
@@ -16,10 +14,7 @@ export const BackendApiFactory = ApiMakeFactory({
   getOrigin: () => configOriginValue,
   dataMapper: (json) => backendJsonApiSync(json as BackendJsonApiPayloadInterface),
   alterHeaders: (headers) => {
-    // TODO-FE[TPNX-3006] remove this headers once web-app search auth is allowed using other authentication mechanizm
-    headers['X-Utgz'] = apiCookieAuthenticatorSignCookie(captchaAuthenticatorCookie);
-    headers['cookie'] = `captcha_authenticator=${captchaAuthenticatorCookie}`;
-    headers['user-agent'] = 'PostmanRuntime/1';
+    headers['user-agent'] = SECRET_PF_USER_AGENT;
   },
   requireAuth: false,
 });
