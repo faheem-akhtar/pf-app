c51316bef123c544d9fd91e8d576abca7460cdb6
Batuhan Toparlak <78409208+pf-batu@users.noreply.github.com>
2021-08-31 13:19:44 +0300

    [CX-370] prevent removing www from configOriginIfDevUseStagingValue (#162)

--- src/config/origin/if-dev-use-staging-value.ts
+++ src/config/origin/if-dev-use-staging-value.ts
@@ -2,5 +2,5 @@ import { configOriginValue } from './value';
 import { helpersIsDevelopment } from 'helpers/is-development';
 
 export const configOriginIfDevUseStagingValue = (): string => {
-  return configOriginValue.replace('https://www.', `https://${helpersIsDevelopment ? 'staging.' : ''}`);
+  return helpersIsDevelopment ? configOriginValue.replace('https://www.', 'staging.') : configOriginValue;
 };
