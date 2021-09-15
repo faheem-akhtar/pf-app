0c596d0538aed39754bd9ee2ae3017b380a0af5e
Anıl Karaabalı <anil@propertyfinder.ae>
2021-09-15 10:12:45 +0300

    [CX-502] Add filter sort choices (#187)

--- src/components/filters/data/choices/get-sort.ts
+++ src/components/filters/data/choices/get-sort.ts
@@ -1,8 +1,21 @@
 import { FiltersValueFieldSortChoiceType } from 'components/filters/value/field/sort-choice.type';
+import { FiltersValueInterface } from 'components/filters/value/interface';
 import { FiltersParametersEnum } from 'enums/filters/parameters.enum';
 
+import { FiltersDataInterface } from '../interface';
 import { filtersDataChoicesMakeGetChoices } from './make-get-choices';
 
-export const filtersDataChoicesGetSort = filtersDataChoicesMakeGetChoices<FiltersValueFieldSortChoiceType>(
-  FiltersParametersEnum.sort
-);
+const getSortChoices = filtersDataChoicesMakeGetChoices<FiltersValueFieldSortChoiceType>(FiltersParametersEnum.sort);
+
+export const filtersDataChoicesGetSort = (
+  value: FiltersValueInterface,
+  data: FiltersDataInterface
+): FiltersValueFieldSortChoiceType[] => {
+  const allChoices = getSortChoices(value, data);
+
+  if (value[FiltersParametersEnum.isDeveloperProperty]) {
+    return allChoices;
+  }
+
+  return allChoices.filter((choice) => !['da', 'dd'].includes(choice.value));
+};
