cb07880561ce7d652acfd0ab374d915a8944bcc5
Oleg Malkov <74129133+pfolegmalkov@users.noreply.github.com>
2021-10-11 13:57:08 +0400

    [CX-184] manifest.json load fix (#241)

--- src/components/head/component.tsx
+++ src/components/head/component.tsx
@@ -10,8 +10,8 @@ export const HeadComponent = ({ pageTitle, shouldIndex = true }: HeadComponentPr
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <link rel='icon' type='image/png' sizes='32x32' href='static/favicon/32x32.png' />
       <link rel='icon' type='image/png' sizes='16x16' href='static/favicon/16x16.png' />
-      <link rel='manifest' href='/static/manifest.json' />
-      <link rel='mask-icon' href='/static/favicon/safari-pinned-tab.svg' color='#ef5e4e' />
+      <link rel='manifest' href='/en/static/manifest.json' />
+      <link rel='mask-icon' href='/en/static/favicon/safari-pinned-tab.svg' color='#ef5e4e' />
       {/* Do not render telephone numbers as links */}
       <meta name='format-detection' content='telephone=no' />
       <meta name='robots' content={`${shouldIndex ? '' : 'no'}index,follow`} />
