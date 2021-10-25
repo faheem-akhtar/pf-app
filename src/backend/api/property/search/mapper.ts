05f03b0884758aeeecdb9a98199dd3362a874435
Anıl Karaabalı <anil@propertyfinder.ae>
2021-10-25 12:11:32 +0300

    [CX-838] Fix duplication in search result properties (#300)
    * [CX-838] Fix duplication in search result properties
    
    * [CX-838] Refactor duplication
    
    Co-authored-by: Faheem Akhtar <faheem-akhtar@users.noreply.github.com>
    Co-authored-by: Gomathy Dhanasekaran <76898752+GomathyDhanasekaran@users.noreply.github.com>

--- src/backend/api/property/search/mapper.ts
+++ src/backend/api/property/search/mapper.ts
@@ -13,92 +13,98 @@ export const backendApiPropertySearchMapper = (
   data: BackendApiPropertySearchJsonApiResultType,
   rawJson: BackendApiPropertySearchRawJsonResponseType
 ): PropertySerpSearchResultType => {
-  const properties: PropertySerpInterface[] = [
-    ...data.cts,
-    ...data.smart_ads,
-    ...data.direct_from_developer,
-    ...data.properties,
-  ].map((property) => {
-    const {
-      area,
-      name,
-      links,
-      verified,
-      listing_level,
-      bathroom_value,
-      bedroom_value,
-      quality_score,
-      location_tree_path,
-      completion_status,
-      location_tree,
-      exclusive,
-      property_type,
-      meta,
-      size,
-      reference,
-      date_insert,
-      default_price,
-    } = property;
+  const { properties } = [...data.cts, ...data.smart_ads, ...data.direct_from_developer, ...data.properties].reduce<{
+    properties: PropertySerpInterface[];
+    set: Set<string>;
+  }>(
+    (acc, property) => {
+      const {
+        area,
+        name,
+        links,
+        verified,
+        listing_level,
+        bathroom_value,
+        bedroom_value,
+        quality_score,
+        location_tree_path,
+        completion_status,
+        location_tree,
+        exclusive,
+        property_type,
+        meta,
+        size,
+        reference,
+        date_insert,
+        default_price,
+      } = property;
 
-    const { listed_at_message, live_event_metadata, video_metadata } = meta;
-    const { email, phone, whatsapp } = meta.contact_options.app;
+      const { listed_at_message, live_event_metadata, video_metadata } = meta;
+      const { email, phone, whatsapp } = meta.contact_options.app;
 
-    const contactOptionsList: PropertyContactOptionsListInterface = { email: !!email };
+      const contactOptionsList: PropertyContactOptionsListInterface = { email: !!email };
 
-    if (phone) {
-      contactOptionsList.phone = phone;
-    }
-
-    if (whatsapp) {
-      contactOptionsList.whatsapp = whatsapp;
-      if (configPropertyHideEmailIfWhatsappPresent) {
-        contactOptionsList.email = false;
+      if (phone) {
+        contactOptionsList.phone = phone;
       }
-    }
 
-    const propertyCompact: PropertySerpInterface = {
-      area,
-      liveEvent: !!live_event_metadata,
-      publishDate: listed_at_message,
-      name,
-      verified,
-      listingLevel: listing_level,
-      bathroomValue: bathroom_value,
-      bedroomValue: bedroom_value,
-      locationTreePath: location_tree_path,
-      exclusive,
-      url: links.self,
-      imgUrl: links.image_property_medium,
-      // TODO-FE[CX-373] remove imgUrlSmall
-      imgUrlSmall: links.image_property_small,
-      imagesCount: meta.images_count,
-      propertyTypeName: property_type.name,
-      locationTreeCompact:
-        location_tree?.map((location) => ({
-          id: location.id,
-          name: location.name,
-          location_type: location.location_type,
-        })) || [],
-      contactOptionsList,
-      priceText: meta.price_text,
-      qualityScore: quality_score,
-      dateInsert: new Date(date_insert).toDateString(),
-      id: property.id,
-      completionStatus: completion_status,
-      reference,
-      size,
-      agentId: property.agent?.id || '',
-      brokerId: property.broker?.id || '',
-      defaultPrice: default_price,
-      videoTour: video_metadata,
-    };
+      if (whatsapp) {
+        contactOptionsList.whatsapp = whatsapp;
+        if (configPropertyHideEmailIfWhatsappPresent) {
+          contactOptionsList.email = false;
+        }
+      }
 
-    if (configIsTrace) {
-      (propertyCompact as unknown as Record<string, Object>).__full = JSON.parse(JSON.stringify(property));
-    }
+      const propertyCompact: PropertySerpInterface = {
+        area,
+        liveEvent: !!live_event_metadata,
+        publishDate: listed_at_message,
+        name,
+        verified,
+        listingLevel: listing_level,
+        bathroomValue: bathroom_value,
+        bedroomValue: bedroom_value,
+        locationTreePath: location_tree_path,
+        exclusive,
+        url: links.self,
+        imgUrl: links.image_property_medium,
+        // TODO-FE[CX-373] remove imgUrlSmall
+        imgUrlSmall: links.image_property_small,
+        imagesCount: meta.images_count,
+        propertyTypeName: property_type.name,
+        locationTreeCompact:
+          location_tree?.map((location) => ({
+            id: location.id,
+            name: location.name,
+            location_type: location.location_type,
+          })) || [],
+        contactOptionsList,
+        priceText: meta.price_text,
+        qualityScore: quality_score,
+        dateInsert: new Date(date_insert).toDateString(),
+        id: property.id,
+        completionStatus: completion_status,
+        reference,
+        size,
+        agentId: property.agent?.id || '',
+        brokerId: property.broker?.id || '',
+        defaultPrice: default_price,
+        videoTour: video_metadata,
+      };
 
-    return propertyCompact;
-  });
+      if (configIsTrace) {
+        (propertyCompact as unknown as Record<string, Object>).__full = JSON.parse(JSON.stringify(property));
+      }
+
+      if (!acc.set.has(property.id)) {
+        acc.properties.push(propertyCompact);
+        acc.set.add(property.id);
+      }
+
+      return acc;
+    },
+    { properties: [], set: new Set() }
+  );
 
   const result: PropertySerpSearchResultType = {
     title: data.meta.meta_title,
