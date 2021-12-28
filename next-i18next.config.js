b4ee06259e62793969ab996d4ae1ccc6ada7d366
Faheem Akhtar <faheem-akhtar@users.noreply.github.com>
2021-12-28 14:30:54 +0400

    [CX-1093] - Site should work without default language (#374)
    * [CX-1093] - Set the default language based on country instead of underscore
    
    * [CX-1093] - handle the case where country env is not available
    
    * [CX-1093] - Redirect serp and landing pages to pf-web-app that don't have language
    
    * [CX-1093] - fix the typo
    
    Co-authored-by: Dolly Arora <dolly@propertyfinder.ae>

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const countryCode = process.env.NEXT_PUBLIC_COUNTRY_CODE?.toLowerCase();
+// Default locale should be updated once we migrate to Next.js 12 by using Middleware.
+const defaultLocale = {
+  ae: 'en',
+  sa: 'ar',
+  eg: 'ar',
+  bh: 'en',
+  qa: 'en',
+  lb: 'en',
+  ma: 'fr',
+};
 
 module.exports = {
   i18n: {
     locales: ['en', 'ar', 'fr', '_'],
-    defaultLocale: '_',
+    defaultLocale: defaultLocale[countryCode] || '_',
   },
   localePath: path.resolve('./public/static/translations'),
   react: {
