2a685ae92f0e9d08ecc68495dbd5ac7876fa5ffc
Faheem Akhtar <faheem-akhtar@users.noreply.github.com>
2022-01-03 15:59:31 +0400

    [CX-1083] - Release landing pages for KSA (#373)
    * [CX-1083] - Release landing pages for KSA
    
    * [CX-1083] - apply 301 redirection for some specific location slugs
    
    * [CX-1083] - fix the primary language param bug
    
    Co-authored-by: Mesut KURTULAN <94890054+MesutKurtulanPF@users.noreply.github.com>

--- src/backend/api/location/slug-history/fetcher.ts
+++ src/backend/api/location/slug-history/fetcher.ts
@@ -1,20 +1,19 @@
 import { BackendApiFactory } from 'backend/api/factory';
 import { BackendModelLocationSlugHistoryInterface } from 'backend/model/location/slug-history.interface';
-import { configCommon } from 'config/common';
 import { LocationSlugHistoryInterface } from 'types/location/slug-history.interface';
 
 import { backendApiLocationSlugHistoryMapper } from './mapper';
 
 export const backendApiLocationSlugHistoryFetcher = ({
-  locale,
+  isPrimaryLocale,
 }: {
-  locale?: string;
+  isPrimaryLocale?: boolean;
 }): ReturnType<typeof BackendApiFactory> =>
   BackendApiFactory<LocationSlugHistoryInterface[], BackendModelLocationSlugHistoryInterface[]>({
     method: 'GET',
     url: 'location-slug-history',
     queryDefaultParams: {
-      ['filter[primary_language]']: configCommon.language.current === locale,
+      ['filter[primary_language]']: !!isPrimaryLocale,
     },
     dataMapper: backendApiLocationSlugHistoryMapper,
   });
